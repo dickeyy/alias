deba042ccd68958d20ac7a9c560e6b5d162a1fa5
Kyle Dickey <k@kyle.so>
2026-03-10 11:25:40 -0600

    delete everything

--- lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient(
-    "https://tvuqsddnghluubucrmxo.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2dXFzZGRuZ2hsdXVidWNybXhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNDg3MTEsImV4cCI6MjA2ODcyNDcxMX0._DkpUbdzv-quEIe1qwrXUB5mYUJTARdecQTmoa8285s"
-);
-
-export { supabase };
